e9ebf5522b5ed12f58d46fbe8fa5d738c22c99c6
Ariful <arifulmit17@gmail.com>
2025-08-17 23:41:48 +0600

    added problem 9

---
+++ problem-9.js
@@ -0,0 +1,10 @@
+function fibb(number){
+    for (let i=1;i<=number;i++){
+        product=product*i
+    }
+    return product
+}
+const number=5
+let product=1
+product = fibb(number)
+console.log(product);
