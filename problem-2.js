a2904ad52c242375313d5fe59cb14d99f04e59a1
Ariful <arifulmit17@gmail.com>
2025-08-17 23:10:56 +0600

    added problem 2

---
+++ problem-2.js
@@ -0,0 +1,15 @@
+function vowel(str1){
+    for (let i of str1){
+        if (i=='a' || i=='e' || i=='i' || i=='o'||i=='u' ){
+            count=count+1
+        }
+        
+    }
+    return count
+
+}
+const str1="programming"
+let count=0
+const vowels=['a','e','i','o','u']
+let number_of_vowel=vowel(str1)
+console.log(number_of_vowel);
