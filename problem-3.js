02c6488e92dda3a769dc7ef3055244eac88ccabe
Ariful <arifulmit17@gmail.com>
2025-08-18 12:49:41 +0600

    added problem 3

---
+++ problem-3.js
@@ -0,0 +1,16 @@
+function palindrome(str1){
+    for(i=str1.length-1;i>=0;i--){
+        str2=str2+str1[i]
+    }
+    if(str1===str2){
+        return true
+    }else{
+        return false
+    }
+
+}
+const str1='madam'
+// const str1='hello'
+let str2=''
+let result=palindrome(str1)
+console.log(result);
